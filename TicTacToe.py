ac7559af57f18307934763273beb2cec40231d9a
DomiSchr <domischroeder@gmx.de>
2021-02-16 22:29:14 +0100

    TicTacToe.py aktualisiert

--- TicTacToe.py
+++ TicTacToe.py
@@ -8,32 +8,49 @@ def ausg():
 def gewinncheck(x):
 		#Zeile:
 	if (spielfeld[0] == x) and (spielfeld[1] == x) and (spielfeld[2] == x):
-		return True
+		return False
 	elif (spielfeld[3] == x) and (spielfeld[4] == x) and (spielfeld[5] == x):
-		return True
+		return False
 	elif (spielfeld[6] == x) and (spielfeld[7] == x) and (spielfeld[8] == x):
-		return True
+		return False
 		#Spalten:
 	elif (spielfeld[0] == x) and (spielfeld[3] == x) and (spielfeld[6] == x):
-		return True
+		return False
 	elif (spielfeld[1] == x) and (spielfeld[4] == x) and (spielfeld[7] == x):
-		return True
+		return False
 	elif (spielfeld[2] == x) and (spielfeld[5] == x) and (spielfeld[8] == x):
-		return True
+		return False
 		#Diagonal:
 	elif (spielfeld[0] == x) and (spielfeld[4] == x) and (spielfeld[8] == x):
-		return True
-	elif (spielfeld[2] == x) and (spielfeld[4] == x) and (spielfeld[6] == x):
-		return True
-	else:
 		return False
+	elif (spielfeld[2] == x) and (spielfeld[4] == x) and (spielfeld[6] == x):
+		return False
+	else:
+		return True
 
-#input = input("Spieler 'x' Feld eingeben: ")
-#spielfeld[input - 1] = "x"
+def spielen(x):
+	s1 = "Spieler: "
+	s2 = " Feld eingeben: "
+	inp = input( s1 + x + s2)
+	spielfeld[inp - 1] = x
+
+gewinn = True
+spieler = "x"
+
+while gewinn:
+	ausg()
+	spielen(spieler)
+
+	gewinn = gewinncheck(spieler)
+
+	if spieler == "x":
+		spieler = "o"
+	else:
+		spieler = "x"
+
+
+print("Spieler " + spieler + " hat gewonnen!")
+ausg()
+
 
 spielfeld = ["x", " ", " ", " ", "x", " ", " ", " ", "x"]
-
-print(gewinncheck("x"))
-
-
-ausg()
